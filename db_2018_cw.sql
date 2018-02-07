616b1b676fa2d646989d27d29df0a6f3c3aca65d
Vasilios Rallis <vgr16@tui.doc.ic.ac.uk>
2018-02-07 13:35:49 +0000

    Updated db_cw1

--- db_2018_cw.sql
+++ db_2018_cw.sql
@@ -8,40 +8,45 @@ ON person_mother.name = person_child.mother
 WHERE person_child.dod < person_father.dod AND person_child.dod < person_mother.dod
 ORDER BY name;*/
 
+/*
 SELECT name
 FROM person
-WHERE person.dod < ALL(SELECT person_parent.dod
-                       FROM person AS person_parent
-                       WHERE person_parent.name = person.father
-                       OR person_parent.name = person.mother)
-AND person.dod IS NOT NULL
-AND person.father IS NOT NULL
-AND person.mother IS NOT NULL
+WHERE EXISTS (SELECT *
+              FROM person AS person_father, person AS person_mother
+              WHERE person.father = person_father.name
+              AND person.mother = person_mother.name
+              AND person.dod < person_mother.dod
+              AND person.dod < person_father.dod)
 ORDER BY name;
-
--- Q2 returns (name)
+*/
+                                      
+-- Q2 return (name)
 /*SELECT name
 FROM monarch
+WHERE house IS NOT NULL
 UNION
 SELECT name
-FROM prime_minister;*/
-
+FROM prime_minister;
+*/
 -- Q3 returns (name)
 /*SELECT monarch.name 
 FROM person JOIN monarch ON person.name = monarch.name
 WHERE EXISTS (SELECT *
               FROM monarch AS monarch_successor
               WHERE monarch_successor.accession < person.dod
-              AND monarch_successor.accession > monarch.accession);*/
+              AND monarch_successor.accession > monarch.accession)
+AND monarch.house IS NOT NULL;
+*/
 
 -- Q4 returns (house,name,accession)
 /*SELECT house, name, accession
 FROM monarch
-WHERE accession >= ALL (SELECT monarch_successor.accession
-                        FROM monarch AS monarch_successor
-                        WHERE monarch.house = monarch_successor.house)
-AND house IS NOT NULL
-ORDER BY accession;*/
+WHERE  accession < ALL(SELECT monarch_successor.accession
+                    FROM monarch AS monarch_successor
+                    WHERE (monarch.house <> monarch_successor.house) IS NOT TRUE
+                    AND monarch_successor.name <> monarch.name)
+ORDER BY accession;
+*/
 
 -- Q5 returns (first_name,popularity)
 
